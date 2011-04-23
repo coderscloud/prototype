d38746f33b8f89327425d21f50dc0b1e84e67781
Hichem <hichem.njeh@gmail.com>
2011-04-23 17:56:16 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodersCloud::Application.config.secret_token = '1bfddb69c5a241aea4435417962817e4ee2fd62b701ec3ff57a58792da1f7fcb6b3361518f6788e0951374edcc26fb46084a60143154e39516d90fe1e7fda95e'
